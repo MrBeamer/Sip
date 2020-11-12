278c2918c6a66b442d0646371c1cd68ab99851d9
michael beamer <beamermichael@gmail.com>
2020-11-12 18:10:11 +0100

    added delete function

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'doses/index'
-  get 'doses/show'
-  get 'doses/new'
-  get 'doses/create'
-  get 'doses/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
 end
