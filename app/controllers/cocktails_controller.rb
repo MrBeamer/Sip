19ec83a4c619585309fc8d55a1a132d8b1b901eb
michael beamer <beamermichael@gmail.com>
2020-11-12 15:19:00 +0100

    added the doses controller

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,47 @@
+class CocktailsController < ApplicationController
+  before_action :find_cocktail, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def edit
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to @cocktail, notice: 'Cocktail was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @cocktail.update(cocktail_params)
+      redirect_to @cocktail, notice: 'Cocktail was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'doses/index'
+  get 'doses/show'
+  get 'doses/new'
+  get 'doses/create'
+  get 'doses/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cocktails
 end
